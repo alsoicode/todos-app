f8b51952f08decc52020636a74221c7179623cc9
Brandon Taylor <btaylorweb@gmail.com>
2013-07-15 22:29:16 -0500

    Removed un-needed model managers for todos.

--- todos/models.py
+++ todos/models.py
@@ -2,27 +2,11 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
-class CompletedTodoManager(models.Manager):
-    def get_query_set(self):
-        return super(CompletedTodoManager, self).get_query_set().filter(
-            completed_on__isnull=False)
-
-
-class UncompletedTodoManager(models.Manager):
-    def get_query_set(self):
-        return super(UncompletedTodoManager, self).get_query_set().filter(
-            completed_on__isnull=True)
-
-
 class Todo(models.Model):
     user = models.ForeignKey(User, editable=False)
     title = models.CharField(max_length=255)
     created_on = models.DateTimeField(auto_now_add=True)
     completed_on = models.DateTimeField(blank=True, null=True)
-
-    objects = models.Manager()
-    complete = CompletedTodoManager()
-    uncomplete = UncompletedTodoManager()
 
     def __unicode__(self):
         return self.title
